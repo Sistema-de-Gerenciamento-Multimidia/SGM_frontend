d09e3ba691848f2ccd63a94cd46276d79e266467
FerdRosa <fernando.a.rosa02@gmail.com>
2024-11-24 16:51:49 -0300

    Criando o componente Header

--- src/pages/dashboardpage.tsx
+++ src/pages/dashboardpage.tsx
@@ -1,23 +1,9 @@
-import { Link } from "react-router-dom";
-import { FaCog, FaUserCircle } from "react-icons/fa";
+import { Header } from "../components/header"
 
 export function DashboardPage() {
   return (
     <div className="flex flex-col h-screen bg-gray-100">
-      <header className="bg-white shadow-md p-4 flex items-center justify-between">
-        <h1 className="text-2xl font-bold text-fulvouscolor">Nome do App</h1>
-        <input
-          type="text"
-          placeholder="Pesquisar arquivos..."
-          className="flex-grow mx-4 px-3 py-2 rounded border border-gray-300 focus:outline-none focus:border-fulvoushover"
-        />
-        <div className="flex items-center space-x-4">
-          <FaCog className="text-gray-600 text-2xl cursor-pointer hover:text-fulvoushover" />
-          <Link to="/perfil">
-            <FaUserCircle className="text-gray-600 text-2xl cursor-pointer hover:text-fulvoushover" />
-          </Link>
-        </div>
-      </header>
+      <Header />
 
       
       <main className="flex-grow p-6">
--- src/pages/perfilpage.tsx
+++ src/pages/perfilpage.tsx
@@ -1,32 +1,18 @@
 import { useState } from 'react';
-import { FaCog, FaUserCircle } from "react-icons/fa";
-import { Link } from "react-router-dom";
 import Perfil from "../../public/robot_perfil.png";
 import { Galeria } from '../components/perfil_galeria';
 import { Fotos } from '../components/perfil_fotos';
 import { Videos } from '../components/perfil_videos';
 import { Audio } from '../components/perfil_audio';
+import { Header } from "../components/header"
 
 export function PerfilPage() {
   const [activeTab, setActiveTab] = useState('Galeria');
 
   return (
     <div className="flex flex-col h-screen bg-gray-100">
-      {/* Cabeçalho */}
-      <header className="bg-white shadow-md p-4 flex items-center justify-between">
-        <h1 className="text-2xl font-bold text-fulvouscolor">Nome do App</h1>
-        <input
-          type="text"
-          placeholder="Pesquisar arquivos..."
-          className="flex-grow mx-4 px-3 py-2 rounded border border-gray-300 focus:outline-none focus:border-fulvoushover"
-        />
-        <div className="flex items-center space-x-4">
-          <FaCog className="text-gray-600 text-2xl cursor-pointer hover:text-fulvoushover" />
-          <Link to="/perfil">
-            <FaUserCircle className="text-gray-600 text-2xl cursor-pointer hover:text-fulvoushover" />
-          </Link>
-        </div>
-      </header>
+    
+        <Header />
 
       {/* Conteúdo Principal */}
       <main className="flex-grow p-6">
