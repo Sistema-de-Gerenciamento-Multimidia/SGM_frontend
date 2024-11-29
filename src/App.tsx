28168d379fa86b6adcb7b906bca5b52f3a3e285b
carlosEduardo-hub <carloseduardo010616@gmail.com>
2024-11-29 00:18:06 -0300

    Iniciando lógica de privatização de rota

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import { InicialPageLogin } from "./pages/inicialpagelogin";
 import { DashboardPage } from "./pages/dashboardpage";
-import { PerfilPage } from "./pages/perfilpage"
+import { PerfilPage } from "./pages/perfilpage";
 import { PasswordRecuperation } from "./pages/password_recuperation";
 import { InicialPageRegister } from "./pages/inicialpageregister";
+
+
+const isAuthenticated = () => {
+ 
+  return localStorage.getItem("authToken") !== null;
+};
+
+// Componente para proteger rotas
+const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
+  return isAuthenticated() ? element : <Navigate to="/" replace />;
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <InicialPageLogin />
+    element: <InicialPageLogin />,
   },
   {
     path: "/register",
-    element: <InicialPageRegister />
+    element: <InicialPageRegister />,
   },
   {
     path: "/dashboard",
-    element: <DashboardPage />,
+    element: <ProtectedRoute element={<DashboardPage />} />,
   },
   {
     path: "/perfil",
-    element: <PerfilPage />
+    element: <ProtectedRoute element={<PerfilPage />} />,
   },
   {
     path: "/password_recuperation",
-    element: <PasswordRecuperation />
-  }
-])
+    element: <ProtectedRoute element={<PasswordRecuperation />} />,
+  },
+]);
 
 export function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
-
-
