cc4d36771ac9cfe2e7cf6bb3dbaf14346ee3b6cb
carlosEduardo-hub <carloseduardo010616@gmail.com>
2025-01-20 17:32:03 -0300

    feat:update file card

---
+++ src/components/file_infomations.tsx
@@ -0,0 +1,25 @@
+
+
+export function FileInfomations() {
+  return (
+    <>
+      <div className="max-w-full overflow-hidden">
+        <p className="break-words overflow-wrap">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio
+          dolorem magnam fuga nostrum quaerat at tempora. Ea eius repellendus
+          corporis, maxime et quibusdam necessitatibus exercitationem culpa quod
+          odio, sapiente aspernatur!
+        </p>
+      </div>
+      <hr className="w-full border-gray-200" />
+      <footer>
+        <ul className="flex flex-wrap items-center gap-2 font-serif font-normal opacity-[0.6] max-w-full overflow-hidden">
+          <li>Html</li>
+          <li>Css</li>
+          <li>JavaScript</li>
+          <li>Canvas</li>
+        </ul>
+      </footer>
+    </>
+  );
+}
--- src/pages/dashboardpage.tsx
+++ src/pages/dashboardpage.tsx
@@ -1,21 +1,20 @@
-import { Header } from "../components/header"
+import { Header } from "../components/header";
 // import  { HeroVideoDialogDemoTopInBottomOut }  from "../components/capa_video";
 import { DialogDemo } from "@/components/button_upload";
 import AudioPlayerComponent from "../components/audio_player_component";
+import { FileInfomations } from "@/components/file_infomations";
+import { Pencil, Trash2 } from "lucide-react";
 
 export function DashboardPage() {
   return (
     <div className="flex flex-col min-h-screen bg-primarylemon">
       <Header />
 
-      
       <main className="flex-grow p-6">
-        
         <div className="mb-6">
           <DialogDemo />
         </div>
 
-        
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {[...Array(8)].map((_, index) => (
             <div
@@ -24,12 +23,22 @@ export function DashboardPage() {
             >
               {/* <HeroVideoDialogDemoTopInBottomOut /> */}
               <AudioPlayerComponent
-                src="/audios/musicateste.mp3" 
-                autoPlay={false} 
+                src="/audios/musicateste.mp3"
+                autoPlay={false}
                 loop={false}
                 volume={0.5}
               />
-              <p className="text-gray-600 font-semibold">Arquivo {index + 1}</p>
+              <div className="w-full flex justify-between px-2">
+                <button>
+                  <Pencil size={20} className="text-gray-600 hover:text-gray-800" />
+                </button>
+                <p className="text-gray-600 font-semibold">Arquivo {index + 1}</p>
+                <button>
+                  <Trash2 size={20} className="text-gray-600 hover:text-red-600" />
+                </button>
+              </div>
+              <hr className="w-full border-gray-200" />
+              <FileInfomations />
             </div>
           ))}
         </div>
