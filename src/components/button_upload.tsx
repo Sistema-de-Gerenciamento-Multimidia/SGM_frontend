ea454e1e4999945c5ba93aa82fccedec16a3a8fe
carlosEduardo-hub <carloseduardo010616@gmail.com>
2025-01-20 16:16:11 -0300

    feat:update and validate upload component

--- src/components/button_upload.tsx
+++ src/components/button_upload.tsx
@@ -1,4 +1,4 @@
-import {  useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/dialog";
 import { InputFile } from "./input_file";
 import { useState } from "react";
+import { Label } from "./ui/label";
 
 // Esquema de validação com Zod
 const schema = z.object({
@@ -20,13 +21,19 @@ const schema = z.object({
     .custom<File>((value) => value instanceof File, {
       message: "Você deve selecionar um arquivo.",
     })
-    // .refine((file) => file?.size <= 5 * 1024 * 1024, {
-    //   message: "O arquivo deve ter no máximo 5MB.",
-    // })
     .refine(
       (file) => ["image/jpeg", "image/png", "video/mp4"].includes(file?.type),
       {
         message: "Apenas arquivos JPEG, PNG ou MP4 são permitidos.",
+      }
+    ),
+  description: z.string().min(1, { message: "A descrição é obrigatória." }),
+  tags: z
+    .string()
+    .refine(
+      (value) => value.split(",").filter((tag) => tag.trim()).length > 0,
+      {
+        message: "Adicione pelo menos uma tag separada por vírgulas.",
       }
     ),
 });
@@ -36,6 +43,7 @@ type FormValues = z.infer<typeof schema>;
 
 export function DialogDemo() {
   const {
+    register,
     handleSubmit,
     setValue,
     reset,
@@ -47,7 +55,12 @@ export function DialogDemo() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const onSubmit = (data: FormValues) => {
-    console.log("Arquivo enviado:", data.file);
+    const tagsArray = data.tags.split(",").map((tag) => tag.trim());
+    console.log("Dados enviados:", {
+      file: data.file,
+      description: data.description,
+      tags: tagsArray,
+    });
     reset();
     setIsDialogOpen(false); // Fecha o modal após o envio
   };
@@ -67,17 +80,48 @@ export function DialogDemo() {
           </DialogDescription>
         </DialogHeader>
         <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4 py-4">
-          <div>
-            <InputFile
-              onFileChange={(file: File | null) => {
-                if (file) {
-                  setValue("file", file, { shouldValidate: true });
-                }
-              }}
-            />
-            {errors.file && (
-              <p className="text-red-500 text-sm">{errors.file.message}</p>
-            )}
+          <div className="space-y-4">
+            <div>
+              <InputFile
+                onFileChange={(file: File | null) => {
+                  if (file) {
+                    setValue("file", file, { shouldValidate: true });
+                  }
+                }}
+              />
+              {errors.file && (
+                <p className="text-red-500 text-sm">{errors.file.message}</p>
+              )}
+            </div>
+            <div>
+              <Label htmlFor="description" className="text-right">
+                Descrição
+              </Label>
+              <textarea
+                id="description"
+                {...register("description")}
+                className="w-full px-3 py-2 border rounded outline-none bg-gray-200"
+              />
+              {errors.description && (
+                <p className="text-red-500 text-sm">
+                  {errors.description.message}
+                </p>
+              )}
+            </div>
+            <div>
+              <Label htmlFor="tags" className="text-right">
+                Tags (separadas por vírgulas)
+              </Label>
+              <textarea
+                id="tags"
+                {...register("tags")}
+                className="w-full px-3 py-2 border rounded outline-none bg-gray-200"
+                placeholder="Ex: tag1, tag2, tag3"
+              />
+              {errors.tags && (
+                <p className="text-red-500 text-sm">{errors.tags.message}</p>
+              )}
+            </div>
           </div>
           <DialogFooter>
             <Button
